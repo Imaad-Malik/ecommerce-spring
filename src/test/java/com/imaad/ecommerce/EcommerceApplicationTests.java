6e20f1aa16836bae9f75dba6e153bbdd3417b26c
Imaad Malik <imaadmalik2@gmail.com>
2026-03-26 13:36:24 -0400

    Initial project skeleton.

---
+++ src/test/java/com/imaad/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imaad.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
